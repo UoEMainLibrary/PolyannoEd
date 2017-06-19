0d432f277bf1564fa4324e4bcb6aecb7f551a33c
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-19 18:14:28 +0100

    reformat app.js to standard.js style

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,24 +1,21 @@
 
-////Polyanno Setup
+/// /Polyanno Setup
 
-var loadImage = function() {
-  
-  var theCookieImage; 
+var loadImage = function () {
+  var theCookieImage
   if (isUseless(theCookieImage)) {
-    var thisWebsiteAddress = window.location.href;
-    var theImageID = thisWebsiteAddress.substring( thisWebsiteAddress.indexOf("/editors/") + 9, thisWebsiteAddress.length);
-    imageSelected = "http://images.is.ed.ac.uk/luna/servlet/iiif/"+theImageID+"/info.json";
-  }
-  else {
-    imageSelected = theCookieImage;
+    var thisWebsiteAddress = window.location.href
+    var theImageID = thisWebsiteAddress.substring(thisWebsiteAddress.indexOf('/editors/') + 9, thisWebsiteAddress.length)
+    imageSelected = 'http://images.is.ed.ac.uk/luna/servlet/iiif/' + theImageID + '/info.json'
+  } else {
+    imageSelected = theCookieImage
   };
-  var theImage = getTargetJSON(imageSelected);
-  imageSelectedFormats = theImage.formats;
-  imageSelectedMetadata = theImage.metadata;
+  var theImage = getTargetJSON(imageSelected)
+  imageSelectedFormats = theImage.formats
+  imageSelectedMetadata = theImage.metadata
+}
 
-};
-
-loadImage();
+loadImage()
 
 var polyannoOptionsHTML = `
 
@@ -42,21 +39,20 @@ var polyannoOptionsHTML = `
           <button class="btn btn-default polyanno-report" type="button"><span class="glyphicon glyphicon-exclamation-sign"></span><!--report inappropriate content--></button>
         </div>
 
-`;
-
+`
 
 polyanno_setup({
-  "highlighting": true, 
-  "editor_options": polyannoOptionsHTML
-});
+  'highlighting': true,
+  'editor_options': polyannoOptionsHTML
+})
 
-////Polyglot User Setup
+/// /Polyglot User Setup
 
 var logged_out_user_tab_HTML = `
         <button class="btn btn default">
           <span>Login</span>
         </button>
-`;
+`
 
 var logged_in_users_tab_HTML = `
         <div class="ed-poly-header-obj">
@@ -108,165 +104,148 @@ var logged_in_users_tab_HTML = `
 
         </div>
 
-`;
+`
 
-var searchCookie = function(field) {
-  var fieldIndex = document.cookie.lastIndexOf(field);
-  if (fieldIndex == -1) {  return false;  }
-  else {
-    var postField = document.cookie.substring(fieldIndex+field.length);
-    var theValueEncoded = postField.split(";", 1);
-    var theValue = theValueEncoded[0];
-    return theValue;
+var searchCookie = function (field) {
+  var fieldIndex = document.cookie.lastIndexOf(field)
+  if (fieldIndex == -1) { return false } else {
+    var postField = document.cookie.substring(fieldIndex + field.length)
+    var theValueEncoded = postField.split(';', 1)
+    var theValue = theValueEncoded[0]
+    return theValue
   };
-};
+}
 
-var polyanno_current_username = false;
+var polyanno_current_username = false
 
+var setup_user_basic = function (username) {
+  $('.ed-poly-users-tab').html(logged_in_users_tab_HTML)
 
-var setup_user_basic = function(username) {
+  $('.polyanno-user-username').find('span').html(username)
 
-  $(".ed-poly-users-tab").html(logged_in_users_tab_HTML);
+  // ICON
 
-  $(".polyanno-user-username").find("span").html(username);
+  var current_user_icon = 'http://placekitten.com/g/50/50' // for now
 
-  //ICON
+  $('.polyanno-user-icon').attr('src', current_user_icon)
 
-  var current_user_icon = "http://placekitten.com/g/50/50"; //for now
+  /// FAVOURITES
 
-  $(".polyanno-user-icon").attr("src", current_user_icon);
+  var current_user_favourites_page = '/favourites/' + username // for now because ideally need to use OAuth etc
+  $('.polyanno-user-favourites').attr('href', current_user_favourites_page)
 
-  ///FAVOURITES
+  /// ACTIVITY
 
-  var current_user_favourites_page = "/favourites/"+username; //for now because ideally need to use OAuth etc
-  $(".polyanno-user-favourites").attr("href", current_user_favourites_page);
-
-  ///ACTIVITY
-
-  var current_user_activity_page = "/activities/"+username; //for now because ideally need to use OAuth etc
-  $(".polyanno-user-activity").attr("href", current_user_activity_page);
-
-};
+  var current_user_activity_page = '/activities/' + username // for now because ideally need to use OAuth etc
+  $('.polyanno-user-activity').attr('href', current_user_activity_page)
+}
 
 var enable_favourites = function () {
+  /// //need to update and complete
 
-  /////need to update and complete
-
-  $("#polyanno-page-body").on("click", ".polyanno-favourite", function(event) {
-    var theSpan = $(this).find(".glyphicon");
-    if (theSpan.hasClass("glyphicon-star-empty")) {
-      if ( $(this).closest(".annoPopup").attr("id") == "imageViewer" ) {
-        polyannoAddFavourites("the_image", true);
-      }
-      else {
-        polyannoAddFavourites(textTypeSelected, textSelected);
+  $('#polyanno-page-body').on('click', '.polyanno-favourite', function (event) {
+    var theSpan = $(this).find('.glyphicon')
+    if (theSpan.hasClass('glyphicon-star-empty')) {
+      if ($(this).closest('.annoPopup').attr('id') == 'imageViewer') {
+        polyannoAddFavourites('the_image', true)
+      } else {
+        polyannoAddFavourites(textTypeSelected, textSelected)
       };
-      theSpan.removeClass("glyphicon-star-empty").addClass("glyphicon-star");
-    }
-    else {
-      if ( $(this).closest(".annoPopup").attr("id") == "imageViewer" ) {
-        polyannoRemoveFavourites("the_image", false);
-      }
-      else {
-        polyannoRemoveFavourites(textTypeSelected, textSelected);
+      theSpan.removeClass('glyphicon-star-empty').addClass('glyphicon-star')
+    } else {
+      if ($(this).closest('.annoPopup').attr('id') == 'imageViewer') {
+        polyannoRemoveFavourites('the_image', false)
+      } else {
+        polyannoRemoveFavourites(textTypeSelected, textSelected)
       };
-      polyannoHandleFavourites("removefavourite", textSelected);
-      theSpan.removeClass("glyphicon-star").addClass("glyphicon-star-empty");
+      polyannoHandleFavourites('removefavourite', textSelected)
+      theSpan.removeClass('glyphicon-star').addClass('glyphicon-star-empty')
     };
-  });
+  })
+}
 
-};
+var enable_activity_functionality = function () {
 
-var enable_activity_functionality = function() {
+}
 
-};
+var setup_user_editors = function (username) {
+  setup_user_basic(username)
 
-var setup_user_editors = function(username) {
+  enable_favourites()
 
-  setup_user_basic(username);
+  enable_activity_functionality()
+}
 
-  enable_favourites();
+var redirect_to_home = function () {
 
-  enable_activity_functionality();
+}
 
-};
+var loginAndSetUsername = function () {
+  /// use EASE to setup login and set document.cookie = "theusername="+username+";";
+  // set polyanno_current_username
 
-var redirect_to_home = function() {
+  /// for now
+  polyanno_current_username = 'JaneBlogg'
 
-};
+  setup_user_basic('JaneBlogg')
+}
 
-var loginAndSetUsername = function() {
-  ///use EASE to setup login and set document.cookie = "theusername="+username+";";
-  //set polyanno_current_username
+var logout_basic = function () {
+  $('.ed-poly-users-tab').html(logged_out_users_tab_HTML)
+  document.cookie = 'theusername=' + false + ';'
+  polyanno_current_username = false
+}
 
-  ///for now
-  polyanno_current_username = "JaneBlogg";
-
-  setup_user_basic("JaneBlogg"); 
-  
-};
-
-var logout_basic = function(){
-  $(".ed-poly-users-tab").html(logged_out_users_tab_HTML);
-  document.cookie = "theusername="+false+";";
-  polyanno_current_username = false;
-};
-
-var ed_poly_log_check = function(logged_in_callback, logged_out_callback) {
+var ed_poly_log_check = function (logged_in_callback, logged_out_callback) {
   if (polyanno_current_username != false) {
     if (logged_in_callback != null) {
-      logged_in_callback(polyanno_current_username);
+      logged_in_callback(polyanno_current_username)
     };
-  }
-  else if (searchCookie("theusername") != false) {
-    polyanno_current_username = searchCookie("theusername");
+  } else if (searchCookie('theusername') != false) {
+    polyanno_current_username = searchCookie('theusername')
     if (logged_in_callback != null) {
-      logged_in_callback(polyanno_current_username);
+      logged_in_callback(polyanno_current_username)
     };
-  }
-  else {
-    //logged out
+  } else {
+    // logged out
     if (logged_in_callback != null) {
-      logged_out_callback();
+      logged_out_callback()
     };
   };
+}
 
-};
+// specific to the non-editor pages
+// ed_poly_log_check(setup_user_basic, null);
 
-//specific to the non-editor pages
-//ed_poly_log_check(setup_user_basic, null);
+// specific to editor pages
+// ed_poly_log_check(setup_user_editors, redirect_to_home);
 
-//specific to editor pages
-//ed_poly_log_check(setup_user_editors, redirect_to_home);
+/// //Favourites
 
-/////Favourites
+var polyannoAddFavourites = function (the_favourited_type, the_favourited_id) {
+  var targetData = { 'favourites': { 'image_id': imageSelected } }
+  targetData.favourites[the_favourited_type] = the_favourited_id
 
-var polyannoAddFavourites = function(the_favourited_type, the_favourited_id) {
-  var targetData = {  "favourites": { "image_id" : imageSelected } };
-  targetData.favourites[the_favourited_type] = the_favourited_id;
-
-///////
+/// ////
   $.ajax({
-    type: "PUT",
+    type: 'PUT',
     url: users_url + polyanno_current_username,
-    dataType: "json",
+    dataType: 'json',
     data: targetData
-  });
+  })
+}
 
-};
-
-var polyannoRemoveFavourites = function(the_favourited_type, the_favourited_id) {
-  var targetData = {  "removefavourite": { "image_id" : imageSelected } };
-  targetData.favourites[the_favourited_type] = the_favourited_id;
-///////
+var polyannoRemoveFavourites = function (the_favourited_type, the_favourited_id) {
+  var targetData = { 'removefavourite': { 'image_id': imageSelected } }
+  targetData.favourites[the_favourited_type] = the_favourited_id
+/// ////
   $.ajax({
-    type: "PUT",
+    type: 'PUT',
     url: users_url + polyanno_current_username,
-    dataType: "json",
+    dataType: 'json',
     data: targetData
-  });
-
-};
+  })
+}
 
 /*
 {
@@ -304,76 +283,65 @@ var polyannoRemoveFavourites = function(the_favourited_type, the_favourited_id) 
 }
 */
 
-////Activity
+/// /Activity
 
+/// /PolyEd Queue Setup
 
+var polyEdFilters = null /// /for now
+polyEdCurrentDocs.push(imageSelected)
 
-////PolyEd Queue Setup
+setUpPolyEdChangeBtns()
 
-var polyEdFilters = null; ////for now
-polyEdCurrentDocs.push(imageSelected);
+/// Extracting Image
 
-setUpPolyEdChangeBtns();
+var polyanno_open_the_image_sub_section = function () {
+  var shape = polyanno_map.getLayer(vectorSelected).toGeoJSON()
+  var IIIFsection = getIIIFsectionURL(imageSelected, shape.geometry.coordinates[0], 'jpg')
+  window.open(IIIFsection, 'imageExtract')
+}
 
-///Extracting Image
+/// Downloading Text Files
 
-var polyanno_open_the_image_sub_section = function() {
-  var shape = polyanno_map.getLayer(vectorSelected).toGeoJSON();
-  var IIIFsection = getIIIFsectionURL(imageSelected, shape.geometry.coordinates[0], "jpg");
-  window.open(IIIFsection, "imageExtract");
-};
-
-///Downloading Text Files
-
-//https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications
+// https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications
 
 /*! modernizr 3.3.1 (Custom Build) | MIT *
- * https://modernizr.com/download/?-setclasses !*/
-!function(n,e,s){function o(n){var e=r.className,s=Modernizr._config.classPrefix||"";if(c&&(e=e.baseVal),Modernizr._config.enableJSClass){var o=new RegExp("(^|\\s)"+s+"no-js(\\s|$)");e=e.replace(o,"$1"+s+"js$2")}Modernizr._config.enableClasses&&(e+=" "+s+n.join(" "+s),c?r.className.baseVal=e:r.className=e)}function a(n,e){return typeof n===e}function i(){var n,e,s,o,i,l,r;for(var c in f)if(f.hasOwnProperty(c)){if(n=[],e=f[c],e.name&&(n.push(e.name.toLowerCase()),e.options&&e.options.aliases&&e.options.aliases.length))for(s=0;s<e.options.aliases.length;s++)n.push(e.options.aliases[s].toLowerCase());for(o=a(e.fn,"function")?e.fn():e.fn,i=0;i<n.length;i++)l=n[i],r=l.split("."),1===r.length?Modernizr[r[0]]=o:(!Modernizr[r[0]]||Modernizr[r[0]]instanceof Boolean||(Modernizr[r[0]]=new Boolean(Modernizr[r[0]])),Modernizr[r[0]][r[1]]=o),t.push((o?"":"no-")+r.join("-"))}}var t=[],f=[],l={_version:"3.3.1",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(n,e){var s=this;setTimeout(function(){e(s[n])},0)},addTest:function(n,e,s){f.push({name:n,fn:e,options:s})},addAsyncTest:function(n){f.push({name:null,fn:n})}},Modernizr=function(){};Modernizr.prototype=l,Modernizr=new Modernizr;var r=e.documentElement,c="svg"===r.nodeName.toLowerCase();i(),o(t),delete l.addTest,delete l.addAsyncTest;for(var u=0;u<Modernizr._q.length;u++)Modernizr._q[u]();n.Modernizr=Modernizr}(window,document);
-//////Copied to detect for use of File type because stupid Safari..
+ * https://modernizr.com/download/?-setclasses ! */
+!(function (n, e, s) { function o (n) { var e = r.className, s = Modernizr._config.classPrefix || ''; if (c && (e = e.baseVal), Modernizr._config.enableJSClass) { var o = new RegExp('(^|\\s)' + s + 'no-js(\\s|$)'); e = e.replace(o, '$1' + s + 'js$2') }Modernizr._config.enableClasses && (e += ' ' + s + n.join(' ' + s), c ? r.className.baseVal = e : r.className = e) } function a (n, e) { return typeof n === e } function i () { var n, e, s, o, i, l, r; for (var c in f) if (f.hasOwnProperty(c)) { if (n = [], e = f[c], e.name && (n.push(e.name.toLowerCase()), e.options && e.options.aliases && e.options.aliases.length)) for (s = 0; s < e.options.aliases.length; s++)n.push(e.options.aliases[s].toLowerCase()); for (o = a(e.fn, 'function') ? e.fn() : e.fn, i = 0; i < n.length; i++)l = n[i], r = l.split('.'), r.length === 1 ? Modernizr[r[0]] = o : (!Modernizr[r[0]] || Modernizr[r[0]] instanceof Boolean || (Modernizr[r[0]] = new Boolean(Modernizr[r[0]])), Modernizr[r[0]][r[1]] = o), t.push((o ? '' : 'no-') + r.join('-')) } } var t = [], f = [], l = {_version: '3.3.1', _config: {classPrefix: '', enableClasses: !0, enableJSClass: !0, usePrefixes: !0}, _q: [], on: function (n, e) { var s = this; setTimeout(function () { e(s[n]) }, 0) }, addTest: function (n, e, s) { f.push({name: n, fn: e, options: s}) }, addAsyncTest: function (n) { f.push({name: null, fn: n}) }}, Modernizr = function () {}; Modernizr.prototype = l, Modernizr = new Modernizr(); var r = e.documentElement, c = r.nodeName.toLowerCase() === 'svg'; i(), o(t), delete l.addTest, delete l.addAsyncTest; for (var u = 0; u < Modernizr._q.length; u++)Modernizr._q[u](); n.Modernizr = Modernizr }(window, document))
+/// ///Copied to detect for use of File type because stupid Safari..
 
-
-var generate_export_text = function(editorString, text_type) {
+var generate_export_text = function (editorString, text_type) {
   var the_text = `
-  The image json: `+imageSelected;
-  var the_top_text = $(editorString).find("polyanno-top-voted").html();
+  The image json: ` + imageSelected
+  var the_top_text = $(editorString).find('polyanno-top-voted').html()
   the_text.concat(` 
-    The most popular `+text_type+": "+the_top_text);
+    The most popular ` + text_type + ': ' + the_top_text)
   if ((!isUseless(polyanno_siblingArray)) && (!isUseless(polyanno_siblingArray[0]))) {
-    for (var i=0; i < polyanno_siblingArray.length; i++) {
-      var child = polyanno_siblingArray[i][0];
+    for (var i = 0; i < polyanno_siblingArray.length; i++) {
+      var child = polyanno_siblingArray[i][0]
       var alt_text = `
-      Alternative: `+child.text;
-      the_text.concat(alt_text);
+      Alternative: ` + child.text
+      the_text.concat(alt_text)
     };
   };
   if (!isUseless(vectorSelected)) {
-    var shape = allDrawnItems.getLayer(vectorSelected).toGeoJSON();
-    var IIIFsection = getIIIFsectionURL(imageSelected, shape.geometry.coordinates[0], "jpg");
+    var shape = allDrawnItems.getLayer(vectorSelected).toGeoJSON()
+    var IIIFsection = getIIIFsectionURL(imageSelected, shape.geometry.coordinates[0], 'jpg')
     the_text.concat(` 
-      The image sub-section: `+IIIFsection);
+      The image sub-section: ` + IIIFsection)
   };
   if (Modernizr.filereader) {
-    var generatedFile = new File([the_text], "new_annotation.txt", {type: "text/plain"});
-    var objectURL = URL.createObjectURL(generatedFile);
-    window.open(objectURL, "ExportedText");
+    var generatedFile = new File([the_text], 'new_annotation.txt', {type: 'text/plain'})
+    var objectURL = URL.createObjectURL(generatedFile)
+    window.open(objectURL, 'ExportedText')
   } else {
     // not-supported
-    alert("Get a better browser!!!!!!");
+    alert('Get a better browser!!!!!!')
   };
+}
 
-};
+$('#polyanno-page-body').on('click', '.polyanno-export-text', function (event) {
+  var theEditor = '#' + $(this).closest('.textEditorPopup').attr('id')
+  generate_export_text(polyanno_text_type_selected)
+})
 
-$("#polyanno-page-body").on("click", ".polyanno-export-text", function(event){
-    var theEditor = "#"+ $(this).closest(".textEditorPopup").attr("id");
-    generate_export_text(polyanno_text_type_selected);
-});
-
-
-///Reporting or Flagging Up Inappropriate Content
-
-
-
-
-
-
+/// Reporting or Flagging Up Inappropriate Content
