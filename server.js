e35f5a4e04ae7fc56f116c2ce8a404882e39fa47
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-19 17:42:16 +0100

    Reformat server.js into standardjs style

--- server.js
+++ server.js
@@ -1,117 +1,113 @@
 
 // SETUP REQUIREMENTS
 
-var express    = require('express');
-var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
-//mongoose.Promise = require("bluebird");
-var cookieParser = require('cookie-parser');
-var cors = require('cors');
+var express = require('express')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+// mongoose.Promise = require("bluebird");
+var cookieParser = require('cookie-parser')
+var cors = require('cors')
 
-var polyanno = require('polyanno_storage');
+var polyanno = require('polyanno_storage')
 
-////have released polyanno_storage as a separate NPM package so you should be able to simply "npm install" it the same as the other packages
-///need to set a function to (re)define those variables here within this package rather than just recalling what is defined in the node_modules package...
+/// /have released polyanno_storage as a separate NPM package so you should be able to simply "npm install" it the same as the other packages
+/// need to set a function to (re)define those variables here within this package rather than just recalling what is defined in the node_modules package...
 
-var thisWebsitePort = polyanno.setup.config.app.port;
+var thisWebsitePort = polyanno.setup.config.app.port
 
 // GET APPLICATION RUNNING
 
-var app        = express();  
+var app = express()
 
-////BASIC APP ROUTES & SETUP
+/// /BASIC APP ROUTES & SETUP
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/views'));
-app.use(express.static(__dirname + '/examples'));
+app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/views'))
+app.use(express.static(__dirname + '/examples'))
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/index.html"); 
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
-app.use(cors());
-//Currently using cors for all origins just for development but will need to be specific for actual deployment
+app.use(cors())
+// Currently using cors for all origins just for development but will need to be specific for actual deployment
 
-///////SETUP
+/// ////SETUP
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
-var port = process.env.PORT || thisWebsitePort; 
+var port = process.env.PORT || thisWebsitePort
 
-//////MIDDLEWARE
+/// ///MIDDLEWARE
 
-var editorRouter = express.Router();
+var editorRouter = express.Router()
 
-editorRouter.use(function(req, res, next) {
-    //should have proper logging here 
-    next(); 
-});
+editorRouter.use(function (req, res, next) {
+    // should have proper logging here
+  next()
+})
 
-var userRouter = express.Router();
+var userRouter = express.Router()
 
-userRouter.use(function(req, res, next) {
-    //should have proper logging here 
-    next(); 
-});
+userRouter.use(function (req, res, next) {
+    // should have proper logging here
+  next()
+})
 
-/////////////////PAGE ROUTES
+/// //////////////PAGE ROUTES
 
 editorRouter.get('/:name', function (req, res, next) {
+  /// /specific to University of Edinburgh application to prevent general spamming with any old info.json through the website
+  var fileName = 'http://images.is.ed.ac.uk/luna/servlet/iiif/' + req.params.name + '/info.json'
+  var options = {
+    root: __dirname + '/views/',
+    headers: {
+      'Set-Cookie': 'imageviewing=' + fileName + '; path=/editors ' /// expires todays date???
+    }
+  }
 
-  ////specific to University of Edinburgh application to prevent general spamming with any old info.json through the website
-    var fileName = "http://images.is.ed.ac.uk/luna/servlet/iiif/"+req.params.name+"/info.json";
-    var options = {
-        root: __dirname + '/views/',
-        headers: {
-            'Set-Cookie': "imageviewing="+fileName+"; path=/editors " ///expires todays date???
-        }
-    };
-
-  res.sendFile("theimage.html", options, function (err) {
+  res.sendFile('theimage.html', options, function (err) {
     if (err) {
-      console.log(err);
-      res.status(err.status).end();
+      console.log(err)
+      res.status(err.status).end()
+    } else {
+      // console.log('Sent with '+JSON.stringify(options.headers));
     }
-    else {
-      //console.log('Sent with '+JSON.stringify(options.headers));
-    }
-  });
+  })
+})
 
-});
+editorRouter.use(express.static(__dirname + '/public'))
+editorRouter.use(express.static(__dirname + '/views'))
+editorRouter.use(express.static(__dirname + '/examples'))
 
-editorRouter.use(express.static(__dirname + '/public'));
-editorRouter.use(express.static(__dirname + '/views'));
-editorRouter.use(express.static(__dirname + '/examples'));
+app.get('/beginnertutorial', function (req, res) {
+  res.redirect('/beginnertutorial.html')
+})
 
-app.get('/beginnertutorial', function(req, res) {
-    res.redirect("/beginnertutorial.html"); 
-});
+app.get('/aboutus', function (req, res) {
+  res.redirect('/aboutus.html')
+})
 
-app.get('/aboutus', function(req, res) {
-    res.redirect("/aboutus.html"); 
-});
+/// //////////////USER ROUTES
 
-/////////////////USER ROUTES
+/// need to update to reference a different database
 
-///need to update to reference a different database
+userRouter.get('/username/:username', polyanno.users.getByUsername)
+userRouter.get('/id/:user_id', polyanno.users.getByID)
+userRouter.post('/', polyanno.users.addNew)
+userRouter.put('/:username', polyanno.users.updateOne)
 
-userRouter.get('/username/:username', polyanno.users.getByUsername);
-userRouter.get('/id/:user_id', polyanno.users.getByID);
-userRouter.post('/', polyanno.users.addNew);
-userRouter.put('/:username', polyanno.users.updateOne);
+/// //////GET STARTED
 
-/////////GET STARTED
+app.use('/api', polyanno.router)
+app.use('/editors', editorRouter)
+app.use('/user', userRouter)
 
-app.use('/api', polyanno.router);
-app.use('/editors', editorRouter);
-app.use('/user', userRouter);
+app.use(function (req, res, next) {
+  res.status(404).redirect('/404page.html')
+})
 
-app.use(function(req, res, next) {
-  res.status(404).redirect("/404page.html");
-});
-
-//tells the server where it should be listening for req and res
-app.listen(thisWebsitePort);
-
+// tells the server where it should be listening for req and res
+app.listen(thisWebsitePort)
