8592189b25545ba359a2656757769361c7f07b37
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-20 11:28:39 +0100

    set how the application connect to mongodb through mongoose

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ var polyanno = require('polyanno_storage')
 // import environment variables
 require('dotenv').config({ path: 'variables.env' })
 
+// connect to mongodb and handle bad connection
+mongoose.connect(process.env.DATABASE)
+mongoose.Promise = global.Promise
+mongoose.connection.on('error', (err) => {
+  console.error(`💔💔💔💔💔💔 ➡ ${err.message}`)
+})
+
 // GET APPLICATION RUNNING
 
 var app = express()
