0d1778ad6bfdd6373560d1067078eba06c9c9855
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-21 18:46:43 +0100

    add route for testing polyanno

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('layout', {title: 'PolyannoEd'})
+  res.render('layout')
+})
+
+router.get('/document', (req, res) => {
+  res.render('document', {title: 'Document'})
 })
 
 module.exports = router
