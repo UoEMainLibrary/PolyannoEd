c13138ba47c9df11f3501ecc367d83bb312f056c
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-21 15:33:37 +0100

    change webpack entry point

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ const uglify = new webpack.optimize.UglifyJsPlugin({ // eslint-disable-line
 const config = {
   entry: {
     // we only have 1 entry, but I've set it up for multiple in the future
-    App: './public/javascripts/mvc.js'
+    App: './public/javascripts/polyannoed.js'
   },
   // we're using sourcemaps and here is where we specify which kind of sourcemap to use
   devtool: 'source-map',
