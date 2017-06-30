87b98a35fa3408058ce6212c0795fb04dff2d920
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-30 17:50:48 +0100

    comment selected image for polyanno

--- public/javascripts/modules/PolyannoSetup.js
+++ public/javascripts/modules/PolyannoSetup.js
@@ -1,5 +1,6 @@
 // imageSelected = 'http://images.is.ed.ac.uk/luna/servlet/iiif/UoEwmm~2~2~77099~164515/info.json'
 // imageSelected = 'http://images.is.ed.ac.uk/luna/servlet/iiif/UoEsha~4~4~65346~164252page31/info.json'
+// imageSelected = 'http://images.is.ed.ac.uk/luna/servlet/iiif/UoEsha~4~4~65346~164252page0/info.json'
 
 var polyanno_setup_options = {
   'highlighting': true,
