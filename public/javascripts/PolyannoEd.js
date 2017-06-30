906048eeb1468655808609c386f5a2f43588772e
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-06-30 17:50:01 +0100

    cleanup unused codes

--- public/javascripts/PolyannoEd.js
+++ public/javascripts/PolyannoEd.js
@@ -3,26 +3,8 @@ import '../style/alltheunicode.css'
 import '../style/polyanno.css'
 import '../style/PolyannoEd.css'
 
-import './dragondrop.js'
-import './alltheunicode.js'
-import './polyanno.js'
-import './PolyannoSetup.js'
-import { load } from './utils.js'
-
-$('#savePolyanno').on('click', function () {
-  const recordUndefined = function (k, v) { if (v === undefined) { return null } return v }
-
-  const annotations = JSON.stringify(Polyanno.annotations.getAll(), recordUndefined)
-  const vectors = JSON.stringify(Polyanno.vectors.getAll(), recordUndefined)
-  const translations = JSON.stringify(Polyanno.translations.getAll(), recordUndefined)
-  const transcriptions = JSON.stringify(Polyanno.transcriptions.getAll(), recordUndefined)
-  const editors = JSON.stringify(JSON.decycle(Polyanno.editors.getAll(), true))
-
-  $.post('/save',
-         {vectors: vectors, annotations: annotations, translations: translations, transcriptions: transcriptions, editors: editors},
-          function (data) {
-            location.reload()
-            console.log(data)
-          }
-    )
-})
+// import './dragondrop.js'
+// import './alltheunicode.js'
+// import './polyanno.js'
+// import './PolyannoSetup.js'
+// import { load } from './utils.js'
