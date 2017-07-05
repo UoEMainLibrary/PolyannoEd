13eb3d3bb50e8edc7e2698a555e7da868d48297d
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-07-05 19:03:33 +0100

    change for facebook auth

--- models/User.js
+++ models/User.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const passportLocalMongoose = require('passport-local-mongoose')
-const mongodbErrorHandler = require('mongoose-mongodb-errors')
 const validator = require('validator')
-
-// const UserSchema = new Schema({
-//   username: String,
-//   password: String
-// })
 
 const UserSchema = new Schema({
   email: {
@@ -15,18 +9,17 @@ const UserSchema = new Schema({
     unique: true,
     lowercase: true,
     trim: true,
-    validate: [validator.isEmail, 'Invalid Email Address'],
-    required: 'Please Supply an email address'
+    validate: [validator.isEmail, 'Invalid Email Address']
+    // required: 'Please Supply an email address'
   },
   name: {
     type: String,
-    required: 'Please supply a name',
+    // required: 'Please supply a name',
     trim: true
   },
-  FacebookId: String
+  facebookId: String
 })
 
 UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' })
-UserSchema.plugin(mongodbErrorHandler)
 
 module.exports = mongoose.model('User', UserSchema)
