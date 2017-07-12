2175210d627b0feefb18cb3b7d8d3ee1353ba032
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-07-12 15:12:04 +0100

    fix facebook login request

--- config/passport-strategies/facebook.js
+++ config/passport-strategies/facebook.js
@@ -8,15 +8,24 @@ const fbSettings = {
   clientSecret: process.env.FB_APP_SECRET,
   callbackURL: process.env.FB_CALLBACK_URL,
   scope: ['email'],
+  profileFields: ['id', 'displayName', 'first_name', 'middle_name', 'last_name', 'email', 'photos', 'hometown'],
   enableProof: true,
   passReqToCallback: true
 }
 
 const fbCallback = async (req, accessToken, refreshToken, profile, done) => {
-  User.findOne({ 'facebookId': profile.id }, function (err, user) {
+  console.log('-------profile--------')
+  console.log(profile)
+  const userEmail = profile.emails[0].value
+  await User.findOne({ 'email': userEmail }, async function (err, user) {
     if (err) { return done(err) }
 
     if (user) {
+      const updatedUser = await User.findOneAndUpdate({ 'email': userEmail }, profile, {
+        new: true, // return the new store instead of the old one
+        runValidators: true
+      }).exec()
+      console.log(updatedUser)
       return done(null, user) // user found, return that user
     } else {
       const newUser = new User()
@@ -24,6 +33,8 @@ const fbCallback = async (req, accessToken, refreshToken, profile, done) => {
       newUser.facebookId = profile.id // set the users facebook id
       newUser.name = profile.name.givenName + ' ' + profile.name.familyName // look at the passport user profile to see how names are returned
       newUser.email = profile.emails[0].value // facebook can return multiple emails so we'll take the first
+
+      console.log(newUser)
 
       newUser.save(function (err) {
         if (err) { throw err }
