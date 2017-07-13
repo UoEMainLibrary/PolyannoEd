fa536d91a05a3b23b16ccd49c78e164bcfdb1ed8
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-07-13 15:06:56 +0100

    reformat codes

--- controller/polyannoController.js
+++ controller/polyannoController.js
@@ -4,11 +4,13 @@ exports.saveObjects = async (req, res) => {
   await PolyannoObject.find({imageId: req.body.imageId}).remove()
 
   const objectsSave = await (new PolyannoObject(req.body)).save()
+
   res.send(objectsSave)
 }
 
 exports.getObjects = async (req, res, next) => {
   const imageId = req.params.imageId
+
   const objects = await PolyannoObject.findOne({ imageId: imageId })
 
   if (objects) { res.send(objects) } else { res.send(`[no data] there is no ${imageId}`) }
@@ -16,5 +18,6 @@ exports.getObjects = async (req, res, next) => {
 
 exports.draw = async (req, res, next) => {
   const objects = await PolyannoObject.find()
+
   res.render('document', {title: 'Document', polyannoData: objects, identifier: req.params.identifier, user: req.user})
 }
