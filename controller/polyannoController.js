ee7010ff721befac9dd4db17afb4643092193610
Indra Lukmana <indra.lukmana.il@gmail.com>
2017-07-13 13:54:02 +0100

    remove unused codes

--- controller/polyannoController.js
+++ controller/polyannoController.js
@@ -1,7 +1,6 @@
 const PolyannoObject = require('../models/PolyannoObject')
 
 exports.saveObjects = async (req, res) => {
-  // console.log(req)
   await PolyannoObject.find({imageId: req.body.imageId}).remove()
 
   const objectsSave = await (new PolyannoObject(req.body)).save()
@@ -12,18 +11,10 @@ exports.getObjects = async (req, res, next) => {
   const imageId = req.params.imageId
   const objects = await PolyannoObject.findOne({ imageId: imageId })
 
-  // console.log(`imageId: ${imageId}`)
-  // console.log(objects)
-
-  // if (!objects) return next()
-
   if (objects) { res.send(objects) } else { res.send(`[no data] there is no ${imageId}`) }
-
-  // const objects = await PolyannoObject.find()
-  // res.render('index', {data: objects[0]})
 }
 
 exports.draw = async (req, res, next) => {
   const objects = await PolyannoObject.find()
-  res.render('document', {title: 'Document', polyannoData: objects, identifier: req.params.identifier})
+  res.render('document', {title: 'Document', polyannoData: objects, identifier: req.params.identifier, user: req.user})
 }
